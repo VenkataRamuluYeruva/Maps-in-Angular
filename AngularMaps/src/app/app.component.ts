807d048598c6ce81354749617798fe06d5ebaaf3
venkataramgithub <venkataramulu9100@gmail.com>
2025-02-18 10:27:56 +0530

    'add_V1'.

---
+++ AngularMaps/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { Component } from '@angular/core';
+import {MapDirectionsService} from '@angular/google-maps';
+import { map, Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'AngularMaps';
+  center: google.maps.LatLngLiteral = {lat: 24, lng: 12};
+  zoom = 4;
+
+  readonly directionsResults$: Observable<google.maps.DirectionsResult|undefined>;
+
+  constructor(mapDirectionsService: MapDirectionsService) {
+    const request: google.maps.DirectionsRequest = {
+      destination: {lat: 12, lng: 4},
+      origin: {lat: 14, lng: 8},
+      travelMode: google.maps.TravelMode.DRIVING
+    };
+    this.directionsResults$ = mapDirectionsService.route(request)
+    .pipe(map(response => response.result));
+  }
+}
+
